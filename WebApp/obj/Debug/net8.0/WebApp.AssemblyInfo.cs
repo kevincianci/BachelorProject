4a2144e780492d8158e74558fd57f30d0bf230a7
kevincianci <kevincianci95@gmail.com>
2024-12-20 09:56:01 +0100

    Add Report Generator page and update navigation menu

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0f254bdb324890ae2268941270d847d95f69ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f3e1982c04279c91b9578267e8793a7d774d63")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
